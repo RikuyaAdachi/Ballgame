39e51d1b1ebd34c151ae30834bccc1ed1c9fcbab
Keigo Imai <keigo.imai@gmail.com>
2018-08-02 23:48:45 +0900

    Initial commit -- コミットできないというので代理で

---
+++ Assets/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public UnityEngine.UI.Text ScoreLabel;
+    public GameObject ClearObject;
+    public GameObject TitleButton;
+
+    public void Update()
+    {
+        int count = GameObject.FindGameObjectsWithTag("Pick up").Length;
+        ScoreLabel.text = count.ToString();
+
+        if (count == 0)
+        {
+            // オブジェクトをアクティブにする
+            ClearObject.SetActive(true);
+            TitleButton.SetActive(true);
+        }
+    }
+}
